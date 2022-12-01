44c1e92fe050c01bb562d2d6cf8743cca6fe6749
Renz Steven Madrigal <aaaezel404@gmail.com>
2022-12-02 01:03:54 +0800

    rerere

--- student-official/php/kra1-next.php
+++ student-official/php/kra1-next.php
@@ -17,7 +17,7 @@
 			<h3>Community Development Report</h3>
 		</div>
 		<div class="kra-1">KRA 1: Self-reliant, interdependent, and empowered partner communities and institutions</div>
-		<!------Accordion------------------------->
+		<!-- Accordion -->
 		<div class="accordion accordion-flush" id="accordionFlushExample">
 			<div class="accordion-item">
 				<h2 class="accordion-header" id="flush-headingOne">
@@ -30,13 +30,13 @@
 				aria-controls="flush-collapseOne "
 				>
 				Cultural Life, Expression and Rootedness for all (Culture and heritage development)
-				<!-----Number of DATA submitted------->
+				<!-- Number of DATA submitted -->
 				<div class="header-num-wrapper">
 					<div class="header-num">
 						<input class="form-control" type="number" placeholder="0" readonly />
 					</div>
 				</div>
-				<!-----Number of DATA submitted------->
+				<!-- Number of DATA submitted-->
 				</button>
 				</h2>
 				<div
@@ -46,7 +46,7 @@
 					data-bs-parent="#accordionFlushExample"
 					>
 					<div class="accordion-body">
-						<!-----------------accordion-list ----------------->
+						<!-- accordion-list -->
 						<div class="container">
 							<div class="row">
 								<div class="col"><h3>Status No.</h3></div>
@@ -86,10 +86,10 @@
 										</li>
 									</ul>
 								</div>
-								<!----Targer No.-->
+								<!-- Targer No. -->
 							</div>
 						</div>
-						<!-----------------accordion-list ----------------->
+						<!-- accordion-list -->
 					</div>
 				</div>
 			</div>
@@ -104,13 +104,13 @@
 				aria-controls="flush-collapseTwo"
 				>
 				Sports for all (Sports development)
-				<!-----Number of DATA submitted------->
+				<!-- Number of DATA submitted -->
 				<div class="header-num-wrapper">
 					<div class="header-num">
 						<input class="form-control" type="number" placeholder="0" readonly />
 					</div>
 				</div>
-				<!-----Number of DATA submitted------->
+				<!-- Number of DATA submitted -->
 				</button>
 				</h2>
 				<div
@@ -120,7 +120,7 @@
 					data-bs-parent="#accordionFlushExample"
 					>
 					<div class="accordion-body">
-						<!-----------------accordion-list ----------------->
+						<!-- accordion-list -->
 						<div class="container">
 							<div class="row">
 								<div class="col"><h3>Status No.</h3></div>
@@ -160,10 +160,10 @@
 										</li>
 									</ul>
 								</div>
-								<!----Targer No.-->
+								<!-- Targer No.-->
 							</div>
 						</div>
-						<!-----------------accordion-list ----------------->
+						<!-- accordion-list -->
 					</div>
 				</div>
 			</div>
@@ -179,13 +179,13 @@
 				>
 				Environmental Justice for all (Environmental sustainability and action; disaster risk reduction and
 				management)
-				<!-----Number of DATA submitted------->
+				<!-- Number of DATA submitted -->
 				<div class="header-num-wrapper">
 					<div class="header-num">
 						<input class="form-control" type="number" placeholder="0" readonly />
 					</div>
 				</div>
-				<!-----Number of DATA submitted------->
+				<!-- Number of DATA submitted -->
 				</button>
 				</h2>
 				<div
@@ -195,7 +195,7 @@
 					data-bs-parent="#accordionFlushExample"
 					>
 					<div class="accordion-body">
-						<!-----------------accordion-list ----------------->
+						<!-- accordion-list -->
 						<div class="container">
 							<div class="row">
 								<div class="col"><h3>Status No.</h3></div>
@@ -235,10 +235,10 @@
 										</li>
 									</ul>
 								</div>
-								<!----Target No.-->
+								<!-- Target No. -->
 							</div>
 						</div>
-						<!-----------------accordion-list ----------------->
+						<!-- accordion-list -->
 					</div>
 				</div>
 			</div>
@@ -253,13 +253,13 @@
 				aria-controls="flush-collapseFour"
 				>
 				Holistic Life for all (Family and household development)
-				<!-----Number of DATA submitted------->
+				<!-- Number of DATA submitted -->
 				<div class="header-num-wrapper">
 					<div class="header-num">
 						<input class="form-control" type="number" placeholder="0" readonly />
 					</div>
 				</div>
-				<!-----Number of DATA submitted------->
+				<!-- Number of DATA submitted -->
 				</button>
 				</h2>
 				<div
@@ -269,7 +269,7 @@
 					data-bs-parent="#accordionFlushExample"
 					>
 					<div class="accordion-body">
-						<!-----------------accordion-list ----------------->
+						<!-- accordion-list -->
 						<div class="container">
 							<div class="row">
 								<div class="col"><h3>Status No.</h3></div>
@@ -309,10 +309,10 @@
 										</li>
 									</ul>
 								</div>
-								<!----Targer No.-->
+								<!-- Targer No.-->
 							</div>
 						</div>
-						<!-----------------accordion-list ----------------->
+						<!-- accordion-list -->
 					</div>
 				</div>
 			</div>
@@ -327,13 +327,13 @@
 				aria-controls="flush-collapseFive"
 				>
 				Community organizations formed
-				<!-----Number of DATA submitted------->
+				<!-- Number of DATA submitted -->
 				<div class="header-num-wrapper">
 					<div class="header-num">
 						<input class="form-control" type="number" placeholder="0" readonly />
 					</div>
 				</div>
-				<!-----Number of DATA submitted------->
+				<!-- Number of DATA submitted -->
 				</button>
 				</h2>
 				<div
@@ -343,7 +343,7 @@
 					data-bs-parent="#accordionFlushExample"
 					>
 					<div class="accordion-body">
-						<!-----------------accordion-list ----------------->
+						<!-- accordion-list -->
 						<div class="container">
 							<div class="row">
 								<div class="col"><h3>Status No.</h3></div>
@@ -383,10 +383,10 @@
 										</li>
 									</ul>
 								</div>
-								<!----Targer No.-->
+								<!-- Targer No. -->
 							</div>
 						</div>
-						<!-----------------accordion-list ----------------->
+						<!-- accordion-list -->
 					</div>
 				</div>
 			</div>
@@ -422,32 +422,6 @@
 				</div>
 			</div>
 		</div>
-		<!---javascript bootstrap-->
-		<script
-		src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-		integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-		crossorigin="anonymous"
-		></script>
-		<script
-		src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-		integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz"
-		crossorigin="anonymous"
-		></script>
-		<script
-		src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-		integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-		crossorigin="anonymous"
-		></script>
-		<script
-		src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-		integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-		crossorigin="anonymous"
-		></script>
-		<script
-		src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-		integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-		crossorigin="anonymous"
-		></script>
 	</body>
 </html>
 <script type="text/javascript">
