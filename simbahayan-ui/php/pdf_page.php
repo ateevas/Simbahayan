77206ec4ad249a8fa6411777ff7843ce85daea55
Joshua Cabangal <joshuafujiyama.cabangal.cics@ust.edu.ph>
2022-12-10 16:06:06 +0800

    added pdf_page

---
+++ simbahayan-ui/php/pdf_page.php
@@ -0,0 +1,381 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="http://localhost/Simbahayan/simbahayan-ui/css/pdf_page.css" />
+    <title>KRAs and KPIs</title>
+    <?php
+		include('../cdn/cdn.php');
+		?>
+</head>
+
+<body>
+    <div class="table-container">
+        <div class="table-wrapper">
+            <!-- KRA 1 -->
+            <table class="table table-sm table-bordered ">
+                <thead class="thead-gold">
+                    <tr>
+                        <th scope="col" id="kra">KRA 1</th>
+                        <th scope="col">Self-reliant, interdependent, and empowered partner communities and institutions
+                        </th>
+
+
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <th>Number of Community Development and Advocacy projects under each of the following
+                            categories:
+                        </th>
+
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+
+                        <td scope="row">a. Health for all (Health and wellness development)
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">b. Education for all (Equitable and inclusive education)
+                        </td>
+
+                    </tr>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">c. Good governance for all (Leadership, organizational development, and good
+                            governance)
+                        </td>
+
+
+                    </tr>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">d. Economic justice for all (Employability and social enterprise)
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">e. Christ’s love for all (Socio-pastoral ministry and evangelization)
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">f. Cultural Life, Expression and Rootedness for all (Culture and Heritage
+                            Development
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">g. Sports for all (Sports development)
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">h. Environmental Justice for all (Environmental sustainability and action;
+                            disaster
+                            risk
+                            reduction and management
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            </td>
+                        <td scope="row">i. Holistic Life for all (Family and household development
+
+                            </th>
+                    </tr>
+
+                    <tr>
+                        <th scope="row">2</td>
+                        <th>Number of community organizations formed</th>
+
+                    </tr>
+
+                    <tr>
+                        <th scope="row">3</th>
+                        <th>Number of communities/institutions served</th>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            </td>
+                        <td scope="row">a. Under each project category
+                            </th>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            </td>
+                        <td scope="row">b. Under each community organization formed
+                            </th>
+
+
+                    <tr>
+                        <th scope="row">4</td>
+                        <th>Number of participants/members</th>
+                    </tr>
+                    <tr>
+                        <td scope="row">
+                        </td>
+                        <td scope="row">a. Under each project category
+                            </th>
+
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">b. Under each community organization formed
+                        </td>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <!-- KRA 2 -->
+
+    <div class="table-container">
+        <div class="table-wrapper">
+            <table class="table table-sm table-bordered ">
+                <thead class="thead-light">
+                    <tr>
+                        <th scope="col">KRA 2</th>
+                        <th scope="col">Thomasian engagement as agents of social transformation</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">5</th>
+                        <th>Number of projects under the University Community Development Program (UCDP), particularly
+                            on the
+                            following:</th>
+
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">a. Institutional Development for Community Development
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">b. Social Awareness and Advocacy
+                        </td>
+                    </tr>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">c. Community-Engaged Research
+                        </td>
+                    </tr>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">d. Service-Learning
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row">6</th>
+                        <th>Number of projects under the University Community Development Program (UCDP), particularly
+                            on the
+                            following:</th>
+
+                    </tr>
+
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">a. student organizations
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">b. students
+                        </td>
+                    </tr>
+                    </tr>
+                    <tr>
+                        <td scope="row">
+                            </th>
+                        <td scope="row">c. faculty
+                        </td>
+                    </tr>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">d. administrators
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">e. support staff
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">f. alumni
+                        </td>
+                    </tr>
+
+
+                    <tr>
+                        <th scope="row">7</th>
+                        <th>Number of research presentations in fora/conferences
+                        </th>
+
+                    </tr>
+
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">a. local
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">b. international
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">8</th>
+                        <th>Number of publications
+                        </th>
+
+                    </tr>
+
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">a. local
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">b. international
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <!-- KRA 3 -->
+
+    <div class="table-container">
+        <div class="table-wrapper">
+            <table class="table table-sm table-bordered ">
+                <thead class="thead-light">
+                    <tr>
+                        <th scope="col">KRA 3</th>
+                        <th scope="col">Dynamic Community Development External Partnerships and Networks</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">9</th>
+                        <th>Number of MOA/MOU with various external partner groups:</th>
+
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">a. People’s Organizations
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">b. Government Agencies or LGUs
+
+                        </td>
+                    </tr>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">c. Non- Government Organizations
+
+                        </td>
+                    </tr>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">d. Basic Educational Institutions
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">e. Local Higher Educational Institutions
+
+                    </tr>
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">f. International Higher Educational Institutions
+
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">g. Faith-based/Church-based Organizations
+
+
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row"></th>
+                        <td scope="row">h. Professional/Business Organizations
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row">10</th>
+                        <th>Number of community development and advocacy activities with each external partner
+                            group
+                        </th>
+
+                    </tr>
+
+                    <tr>
+                        <th scope="row">11</th>
+                        <th>Number of training/course offerings on Community Engagement and Organizing</th>
+
+                    </tr>
+
+                    <tr>
+                        <th scope="row">12</th>
+                        <th>Number of participants in training/course offerings on Community Engagement and
+                            Organizing:
+                        </th>
+
+                    </tr>
+
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+
+
+</body>
+
+</html>
