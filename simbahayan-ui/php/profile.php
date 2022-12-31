5953f998f197fc1205dd990b8cc2d0e45fae9654
Joshua Cabangal <joshuafujiyama.cabangal.cics@ust.edu.ph>
2023-01-01 00:41:02 +0800

    designs

--- simbahayan-ui/php/profile.php
+++ simbahayan-ui/php/profile.php
@@ -10,54 +10,72 @@
     <?php
 		include('../cdn/cdn.php');
 		?>
+
 </head>
 
 <body>
     <?php
 		include('../nav_master/nav.php');
 		?>
-    <div class="welcome-container">
-        <div class="welcome-inner">
-            <div class="welcome-context">
-                <h1>Welcome <span id='fname' style="text-transform: capitalize;"></span>!</h1>
-                <p>
-                    You may now access the features of the UST Simbahayan Portal!
-                </p>
+    <div class="background">
+        <div class="welcome-container">
+            <div class="welcome-inner">
+                <div class="welcome-context">
+                    <h1 class="animate__animated animate__fadeInLeft">Welcome <span id='fname'
+                            style="text-transform: capitalize;"></span>!</h1>
+                    <p class="animate__animated animate__fadeInLeft">
+                        You may now access the features of the UST Simbahayan Portal!
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-    <!--bootstrap-->
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
+
+
+        <!--Carousell-->
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators animate__animated animate__fadeInLeft">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+                    aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
+                    aria-label="Slide 5"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../pictures/IMG_2109.JPG" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="../pictures/_MG_4627.JPG" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="../pictures/_MG_4903.JPG" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="../pictures/_MG_7323.JPG" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="../pictures/IMG_2109.JPG" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="../pictures/SIMBAHAYAN-20161208-Cagayan-Relief-06.jpg" class="img-fluid" alt="" />
-            </div>
-            <div class="carousel-item">
-                <img src="../pictures/image 6.png" class="img-fluid" alt="..." />
-            </div>
-            <div class="carousel-item">
-                <img src="../pictures/image 7.png" class="d-block w-100" alt="..." />
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
+
+
+
 </body>
 
 </html>
