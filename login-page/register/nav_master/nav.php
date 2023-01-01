44a5b295f9e870a311b125e1d393a52278c0b69f
Joshua Cabangal <joshuafujiyama.cabangal.cics@ust.edu.ph>
2023-01-02 04:10:27 +0800

    register navbar

--- login-page/register/nav_master/nav.php
+++ login-page/register/nav_master/nav.php
@@ -1,9 +1,35 @@
-<nav class="navbar bg-secondary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="http://localhost/Simbahayan/login-page/php/cd-login.php">
-            <img src="http://localhost/Simbahayan/coordinator/pictures/ustlogo.png" alt="Logo" width="70" height="70"
-                class="d-inline-block align-text-middle" />
-            <span>Simbahayan Portal</span>
-        </a>
+<style type="text/css">
+@font-face {
+    font-family: "Trajan";
+    src: url("http://localhost/Simbahayan/login-page/php/nav_master/Trajan_Pro_Regular.ttf");
+}
+</style>
+<nav class="navbar navbar-light bg-light ">
+    <div class=" container-fluid mb-2 ">
+        <div class="context d-flex flex-row bd-highlight mb-0 m-1 ">
+
+            <div class="mb-0 bd-highlight align-self-center"><a class="navbar-brand" href="#"><img
+                        src="http://localhost/Simbahayan/login-page/pictures/ustlogo.png" alt="" width="70" height="70"
+                        class="d-inline-block align-text-top"></a></div>
+            <div class="mb-0 bd-highlight align-self-center">
+
+            </div>
+
+            <div class="context-container">
+                <div class="context mb-0 bd-highlight align-self-center">
+                    <p class="navbar-specialtext1 mb-0" id="ust">University of Santo Tomas</p>
+                    <p class="navbar-specialtext2 mb-0 h1" id="simbahayan">Simbahayan</p>
+                    <p class="navbar-specialtext1 mb-0" id="comdev">Community Development Office</p>
+                </div>
+            </div>
+            <div class="logo ms-auto">
+                <a class=" navbar-brand" href="#">
+                    <img src="http://localhost/Simbahayan/login-page/pictures/white.png" alt="" width="120" height="70"
+                        class="d-inline-block">
+                </a>
+            </div>
+        </div>
+    </div>
+
     </div>
 </nav>
